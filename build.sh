08db281d9a6f83c5dab66693ba082ba0ee8be8be
Anton Johansson <97antjoh@gmail.com>
2023-03-29 15:28:51 +0200

    Use submodules for glfw dependency

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-clang src/main.c -o ddd -lglfw -lGL -lm
+
+[ ! -d build ] && mkdir build
+
+if [ ! -d external/glfw ]; then
+    git submodule update --init
+    cmake -S external/glfw -B build -DCMAKE_BUILD_TYPE=Release -DGLFW_USE_WAYLAND=On
+    cmake --build build
+fi
+
+clang src/main.c -o ddd build/src/libglfw3.a -lGL -lm
